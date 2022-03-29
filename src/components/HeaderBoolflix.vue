9f9addf1d29661bb38cc801b25407849d8c5750e
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-03-29 10:59:54 +0200

    Initial commit

---
+++ src/components/HeaderBoolflix.vue
@@ -0,0 +1,22 @@
+<template>
+<header>
+  <img :src="logo" alt="">
+</header>
+</template>
+
+<script>
+import logo from '../assets/boolflix.png'
+
+export default {
+  name: 'HeaderVue',
+  data () {
+    return {
+      logo
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
